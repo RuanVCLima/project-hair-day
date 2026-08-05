3fd6b286764e3bdff01e202bc07923018243c21f
Ruan Victor <ruanvclima@gmail.com>
2026-08-05 15:45:01 -0300

    installing babel on the aplication

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
+const CopyWebpackPlugin = require("copy-webpack-plugin")
 
 module.exports = {
     target: "web",
@@ -26,6 +27,14 @@ module.exports = {
         new HtmlWebpackPlugin({
             template:path.resolve(__dirname,"index.html"),
             favicon: path.resolve(__dirname, "src","assets","scissors.svg")
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, "src", "assets"),
+                    to: path.resolve(__dirname, "dist", "src", "assets")
+                }
+            ]
         })
     ],
     module: {
@@ -33,6 +42,16 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
             }
         ]
     }
