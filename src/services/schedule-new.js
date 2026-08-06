f85b5dae5cdaa035a3bace2467c2d077f6f6c030
Ruan Victor <ruanvclima@gmail.com>
2026-08-06 07:55:55 -0300

    configuring the api, and creating a new schedule

---
+++ src/services/schedule-new.js
@@ -0,0 +1,19 @@
+
+import { apiConfig } from "./api-config";
+
+export async function scheduleNew({id, name, when}){
+    try{
+        await fetch(`${apiConfig.baseURL}/schedules`,{
+            method: "POST",
+            headers:{
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify({id,name,when})
+        })
+
+        alert("Agendamento realizado com sucesso")
+    } catch(error){
+        console.log(error)
+        alert("Não foi possível agentar. Tente novamente mais tarde")
+    }
+}
